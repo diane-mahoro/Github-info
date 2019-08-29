62dab36a53b0cfc972d8c54e17e9373f50210c21
diane-mahoro <www.diammah7@gmail.com>
2019-08-29 12:13:44 +0200

    github

--- src/app/gtihub/gtihub.component.ts
+++ src/app/gtihub/gtihub.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ User } from '../user'
 @Component({
   selector: 'app-gtihub',
   templateUrl: './gtihub.component.html',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GtihubComponent implements OnInit {
 
+  user:User[]=[
+    new User("Diane Mahoro"),
+  ];
   constructor() { }
 
   ngOnInit() {
