55b9d4f468ebe4c2c00bda10f84dc9b9241c3b24
diane-mahoro <www.diammah7@gmail.com>
2019-08-29 17:37:03 +0200

    http service

--- src/app/gtihub/gtihub.component.ts
+++ src/app/gtihub/gtihub.component.ts
@@ -9,19 +9,17 @@ import { Repository } from '../repository-class/repository'
 })
 export class GtihubComponent implements OnInit {
 
-  user:User[]=[
-    new User("Diane Mahoro"),
-  ];
+  // user:User;
   repositories: Repository;
   constructor( private http:HttpClient) { }
 
   ngOnInit() {
-    interface repos{
-      name:string;
-      biography: string;
+    interface Repos{
+      login:string;
+      bio: string;
       public_repos: number;
     }
-    this.http.get<repos>("https://api.github.com/users/diane-mahoro?access_token=9ff0b69b6505ff7669adad39ef03ba85828b5d48").subscribe(data=>{
+    this.http.get<Repos>("https://api.github.com/users/diane-mahoro?access_token=9ff0b69b6505ff7669adad39ef03ba85828b5d48").subscribe(data=>{
       this.repositories = new Repository(data.name, data.public_repos, data.biography);
     });
   }
